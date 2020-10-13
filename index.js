83fe9a1fdb7f917708affc883b8378aace2a5785
nettivastaava <vaxandst@cubbli20-gold.local>
2020-10-13 16:11:36 +0300

    build added to back-end

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 
-app.use(cors())
-app.use(express.static('build'))
 
+app.use(express.static('build'))
+app.use(express.json()) 
+app.use(cors())
 morgan.token('body', function (req) {return JSON.stringify(req.body)})
 
-app.use(express.json()) 
 app.use(morgan(' :method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
