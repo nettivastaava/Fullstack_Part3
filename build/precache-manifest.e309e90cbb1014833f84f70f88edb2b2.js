844ef2bba3da0295c72da52f8d81aa6fc7358e19
nettivastaava <vaxandst@cubbli20-gold.local>
2020-10-13 15:44:29 +0300

    build added to back-end

---
+++ build/precache-manifest.e309e90cbb1014833f84f70f88edb2b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a3d868238359ccbfb6535e4852e489",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cab3781ec9edd925d7f",
+    "url": "/static/css/main.5198c9ee.chunk.css"
+  },
+  {
+    "revision": "f6be9ded081ad26c09be",
+    "url": "/static/js/2.8ee96dbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8ee96dbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cab3781ec9edd925d7f",
+    "url": "/static/js/main.acbd2dab.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
